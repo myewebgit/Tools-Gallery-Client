28aa56aaa83b7ac69e9f780259bc3102e9f81942
myewebgit <mnislamctg7@gmail.com>
2022-05-23 17:52:10 +0600

    Updating database

--- src/Pages/Home/Product.js
+++ src/Pages/Home/Product.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Product = ({ product }) => {
     const { id, name, img, description, price, moq, available } = product;
@@ -8,16 +8,19 @@ const Product = ({ product }) => {
     return (
         <div>
 
-            <div class="card w-96 bg-base-100 shadow-xl">
-                <figure><img src={img} alt="Shoes" /></figure>
-                <div class="card-body">
-                    <h5 class="card-title">
+            <div class="card w-96 bg-base-100 shadow-xl ">
+                
+                <img className="h-64 w-64 mx-auto" src={img }  alt="Shoes" />
+                <h5 class="card-title ml-8 text-sm">
                         Name:{name}<br />
                         Price:{price}<br />
                         MOQ:{moq}<br />
-                        Available:{available}
+                        Available:{available}<br/>
+                        Description:{description}
                     </h5>
-                    <p>Description:{description}</p>
+                     
+                <div class="card-body">
+                   
                     <div class="card-actions justify-end">
                         
 
@@ -29,6 +32,9 @@ const Product = ({ product }) => {
                     </div>
                 </div>
             </div>
+
+
+            
         </div>
     );
 };
